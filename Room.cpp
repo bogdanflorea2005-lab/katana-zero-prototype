1b9e6c94b0a318d6e72eda92d64999e049a420e4
Bogda <bogdan.florea2005@gmail.com>
2025-12-04 14:42:23 +0200

    Added more error handling code

--- Room.cpp
+++ Room.cpp
@@ -59,7 +59,7 @@ Room::Room(std::string roomID) {
 
 void Room::drawRoom(sf::RenderWindow &window, Player& player, Camera& camera) {
     if (hasError) {
-        std::string errFile="Textures/PROSTJEGOSFMM.png";
+        std::string errFile="Textures/ThatFuckinFuckThatIHate.png";
         sf::Texture err;
         try {
             if (!err.loadFromFile(errFile)) {
